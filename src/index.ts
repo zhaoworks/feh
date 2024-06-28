a0769a49c11689f06929e692ef8dd133c48bb91f
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2024-06-27 21:10:42 -0300

    feat: create feh fastify plugin (close #2)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-console.log("Hello via Bun!");
+import type { FastifyInstance } from 'fastify';
+import fp from 'fastify-plugin';
+
+interface FehOptions {
+  message: string;
+}
+
+/*
+ * Obs.: this declaration is necessary for including the method `error`
+ * as a valid method of FastifyReply.
+ */
+declare module 'fastify' {
+  interface FastifyReply {
+    error: (status: number, options: FehOptions) => FastifyReply;
+  }
+}
+
+async function plugin(fastify: FastifyInstance) {
+  /*
+   * Obs.: the anonymous function must not be an arrow function,
+   * probably because of the way the Fastify use `this` context.
+   */
+  fastify.decorateReply('error', function (this, status, options) {
+    return this.status(status).send({
+      message: options.message,
+    });
+  });
+}
+
+const feh = fp(plugin, {
+  name: 'feh',
+  fastify: '4.x',
+});
+
+export default feh;
